8fc27e203090a8501d5b6c5238f0eec08e12059d
Yohanes Debebe <yopeman318@gmail.com>
2026-08-03 10:53:48 +0300

    remove signup

--- vector-front-desk-erp-app/src/pages/LoginPage.jsx
+++ vector-front-desk-erp-app/src/pages/LoginPage.jsx
@@ -141,7 +141,7 @@ export default function LoginPage() {
           </button>
         </form>
 
-        <div className="text-center mt-6">
+        {/* <div className="text-center mt-6">
           <button
             onClick={toggleMode}
             className="text-sm text-[#00ced1] hover:underline font-medium"
@@ -150,7 +150,7 @@ export default function LoginPage() {
               ? "Don't have an account? Sign up"
               : 'Already have an account? Sign in'}
           </button>
-        </div>
+        </div> */}
 
         <p className="text-center mt-6 text-[11px] text-gray-300">
           Internal use only &mdash; V☰CTOR ERP System
